39b6cf8f4dd5bdce6720b706fce50f731a677cf4
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2017-08-18 17:12:04 +0200

    Get GeonBit working and make main menu

---
+++ Quoridor/MainMenu.cs
@@ -0,0 +1,57 @@
+﻿using GeonBit.UI;
+using GeonBit.UI.Entities;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quoridor
+{
+    class MainMenu
+    {
+        public MainMenu()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            Panel titlePanel = new Panel(new Vector2(250, 75), PanelSkin.Default, Anchor.TopCenter, new Vector2(0, 5));
+            UserInterface.Active.AddEntity(titlePanel);
+
+            Header title = new Header("Quoridor");
+            titlePanel.AddChild(title);
+            title.SetOffset(new Vector2(0, -10));
+
+            Panel mainPanel = new Panel(new Vector2(500, 500));
+            UserInterface.Active.AddEntity(mainPanel);
+
+            HorizontalLine hline0 = new HorizontalLine();
+            mainPanel.AddChild(hline0);
+            TextInput nameText = new TextInput(false);
+            nameText.PlaceholderText = "Insert name here..";
+            mainPanel.AddChild(nameText);
+            HorizontalLine hline1 = new HorizontalLine();
+            mainPanel.AddChild(hline1);
+
+            TextInput ipText = new TextInput(false);
+            ipText.PlaceholderText = "Insert IP here..";
+            mainPanel.AddChild(ipText);
+            HorizontalLine hline2 = new HorizontalLine();
+            mainPanel.AddChild(hline2);
+
+            TextInput portText = new TextInput(false);
+            portText.PlaceholderText = "Insert port here..";
+            mainPanel.AddChild(portText);
+            HorizontalLine hline3 = new HorizontalLine();
+            mainPanel.AddChild(hline3);
+
+            Button connectButton = new Button("Connect");
+            mainPanel.AddChild(connectButton);
+            connectButton.SetOffset(new Vector2(0, 80));
+            
+        }
+    }
+}
