c593a7c4cc0a08f111698f567518a6db591c4b39
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2017-09-25 19:30:13 +0200

    Utilities added for input

---
+++ Quoridor/Utilities/MouseUtility.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Quoridor
+{
+    static class MouseUtility
+    {
+        static MouseState newState;
+        static MouseState oldState;
+
+        static public bool WasLeftClicked
+        {
+            get { return oldState.LeftButton == ButtonState.Released && newState.LeftButton == ButtonState.Pressed; }
+        }
+
+        static public bool WasRightClicked
+        {
+            get { return oldState.RightButton == ButtonState.Released && newState.RightButton == ButtonState.Pressed; }
+        }
+
+        static public Point Position
+        {
+            get { return newState.Position; }
+        }
+
+        static public void Update()
+        {
+            oldState = newState;
+            newState = Mouse.GetState();
+        }
+    }
+}
