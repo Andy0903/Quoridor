de729ed4f537ad19d19d11b6b4ac021f3ea6ab2f
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2017-09-25 21:10:40 +0200

    Stuck in forever-loop when waiting for input

---
+++ Quoridor/Utilities/InputMessenger.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quoridor
+{
+    public static class InputMessenger
+    {
+        public static void Update()
+        {
+            KeyboardUtility.Update();
+            MouseUtility.Update();
+        }
+
+        public static bool PressedW => KeyboardUtility.WasClicked(Keys.W);
+        public static bool PressedA => KeyboardUtility.WasClicked(Keys.A);
+        public static bool PressedS => KeyboardUtility.WasClicked(Keys.S);
+        public static bool PressedD => KeyboardUtility.WasClicked(Keys.D);
+
+        public static Point MousePosition => MouseUtility.Position;
+        public static bool PressedLMB => MouseUtility.WasLeftClicked;
+    }
+}
